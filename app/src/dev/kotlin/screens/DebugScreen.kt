3e4b98d069c8a21a0adacbd35a62f3c7fd7da2e3
Emmanouil Kounalakis <kounalem@gmail.com>
2024-04-30 14:04:42 +0300

    trim debug screen

--- app/src/dev/kotlin/screens/DebugScreen.kt
+++ app/src/dev/kotlin/screens/DebugScreen.kt
@@ -2,7 +2,6 @@ package screens
 
 import android.app.Activity
 import android.util.Log
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -16,16 +15,18 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import com.kounalem.moviedatabase.core.ui.large
 import com.kounalem.moviedatabase.core.ui.showShowkase
+import com.kounalem.moviedatabase.core.ui.small
 import com.kounalem.moviedatabase.core.ui.xlarge
+import com.kounalem.moviedatabase.managers.FeatureFlags
 import com.kounalem.moviedatabase.network.ServerInfo.Companion.Local
 import com.kounalem.moviedatabase.network.ServerInfo.Companion.Prod
 import com.kounalem.moviedatabase.network.ServerInfo.Companion.Staging
 import com.kounalem.moviedatabase.preferences.PreferenceRepository
-import com.kounalem.moviedatabase.managers.FeatureFlags
 
 @Composable
 fun DebugScreen(
@@ -48,58 +49,76 @@ fun DebugScreen(
             Text(text = "Go to showcase")
         }
 
-        Spacer(modifier = Modifier.padding(start = large, top = large))
-        Text(text = "Select environment:")
+        Spacer(modifier = Modifier.padding(vertical = xlarge))
+        SelectEnvironment(preferenceRepository)
 
-        LazyColumn(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(horizontal = large),
-        ) {
-            itemsIndexed(listOf(Prod, Local, Staging, Prod)) { _, item ->
-                Text(text = item.id,
-                     modifier = Modifier.clickable {
-                         preferenceRepository.insert(PreferenceRepository.ENVIRONMENT, item.id)
-                     })
-            }
-        }
+        Spacer(modifier = Modifier.padding(vertical = xlarge))
+        FeatureFlags(featureFlags)
+    }
 
+}
 
-        val flags = FeatureFlags::class.java.declaredFields.mapNotNull { field ->
-            if (FeatureFlags.FeatureFlag::class.java.isAssignableFrom(field.type)) {
-                field.isAccessible = true
-                field.get(featureFlags) as FeatureFlags.FeatureFlag
-            } else {
-                null
-            }
-        }
-
-        LazyColumn(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(horizontal = large),
-        ) {
-            itemsIndexed(flags) { _, item ->
-
-                Row {
-
-                    val checkedState = remember { mutableStateOf(item.isFlagSet()) }
-                    Text(
-                        text = item.name,
-                    )
-
-                    Switch(
-                        checked = checkedState.value,
-                        onCheckedChange = {
-                            val updated = !checkedState.value
-                            item.setLocalFlag(updated)
-                            checkedState.value = updated
-                        }
-                    )
-                }
+@Composable
+private fun SelectEnvironment(preferenceRepository: PreferenceRepository) {
+    Text(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = large), text = "Select environment:"
+    )
+    val environmentId = preferenceRepository.getString(PreferenceRepository.ENVIRONMENT)
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = large),
+    ) {
+        itemsIndexed(listOf(Prod, Local, Staging, Prod)) { _, item ->
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                val checkedState = remember { mutableStateOf(environmentId == item.id) }
+                Text(text = item.id)
+                Spacer(modifier = Modifier.padding(horizontal = small))
+                Switch(checked = checkedState.value, onCheckedChange = {
+                    preferenceRepository.insert(PreferenceRepository.ENVIRONMENT, item.id)
+                })
             }
         }
     }
+}
 
+@Composable
+private fun FeatureFlags(featureFlags: FeatureFlags) {
+    val flags = FeatureFlags::class.java.declaredFields.mapNotNull { field ->
+        if (FeatureFlags.FeatureFlag::class.java.isAssignableFrom(field.type)) {
+            field.isAccessible = true
+            field.get(featureFlags) as FeatureFlags.FeatureFlag
+        } else {
+            null
+        }
+    }
 
+    Text(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = large),
+        text = "Feature Flags:",
+    )
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = large),
+    ) {
+        itemsIndexed(flags) { _, item ->
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                val checkedState = remember { mutableStateOf(item.isFlagSet()) }
+                Text(
+                    text = item.name,
+                )
+                Spacer(modifier = Modifier.padding(horizontal = small))
+                Switch(checked = checkedState.value, onCheckedChange = {
+                    val updated = !checkedState.value
+                    item.setLocalFlag(updated)
+                    checkedState.value = updated
+                })
+            }
+        }
+    }
 }
