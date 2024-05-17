e25e928ba1f73054cf833a70e2a8b99ee75974da
Emmanouil Kounalakis <kounalem@gmail.com>
2024-05-17 12:59:35 +0300

    move datastore to core

--- core/datastore/src/androidTest/java/com/kounalem/moviedatabase/datastore/ExampleInstrumentedTest.kt
+++ core/datastore/src/androidTest/java/com/kounalem/moviedatabase/datastore/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.kounalem.moviedatabase.network
+package com.kounalem.moviedatabase.datastore
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.kounalem.moviedatabase.network.test", appContext.packageName)
+        assertEquals("com.kounalem.moviedatabase.datastore.test", appContext.packageName)
     }
 }
